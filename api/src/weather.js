c1fb8a831fcd1ac917ee6c94393002794a9499c7
kbph05 <horvatk@shaw.ca>
2025-11-27 16:18:46 -0800

    weather code for weekly condition

--- api/src/weather.js
+++ api/src/weather.js
@@ -35,7 +35,8 @@ export async function getWeatherAPI(lat, lon) {
     const params = {
         latitude: lat,
         longitude: lon,
-        hourly: "temperature_2m",
+        daily: ["temperature_2m_mean", "weather_code"],
+        hourly: "temperature_2m"
     };
    const url = "https://api.open-meteo.com/v1/forecast";
    
@@ -43,6 +44,7 @@ export async function getWeatherAPI(lat, lon) {
         const responses = await fetchWeatherApi(url, params);
         const response = responses[0];
         const hourly = response.hourly();
+        const daily = response.daily();
         const utcOffsetSeconds = response.utcOffsetSeconds();
 
         const weatherData = {
@@ -52,8 +54,15 @@ export async function getWeatherAPI(lat, lon) {
                 ),
                 temperature: hourly.variables(0).valuesArray(),
             },
+            daily: {
+                time: Array.from(
+                    { length: (Number(daily.timeEnd()) - Number(daily.time())) / daily.interval() }, 
+                    (_, i) => new Date((Number(daily.time()) + i * daily.interval() + utcOffsetSeconds) * 1000)
+                ),
+                temperature_mean: daily.variables(0).valuesArray(),
+                weather_code: daily.variables(1).valuesArray(),
+            },
         };
-
         return weatherData;
     } catch (error) {
         console.error("Error fetching weather data:", error);
@@ -73,3 +82,21 @@ export function weatherJSON(data) {
         return null;
     }
 }
+
+// Weather code table:
+/**
+ * Code	            Description
+    0	             Clear sky
+    1, 2, 3	         Mainly clear, partly cloudy, and overcast
+    45, 48	         Fog and depositing rime fog
+    51, 53, 55	     Drizzle: Light, moderate, and dense intensity
+    56, 57	         Freezing Drizzle: Light and dense intensity
+    61, 63, 65	     Rain: Slight, moderate and heavy intensity
+    66, 67	         Freezing Rain: Light and heavy intensity
+    71, 73, 75	     Snow fall: Slight, moderate, and heavy intensity
+    77	             Snow grains
+    80, 81, 82	     Rain showers: Slight, moderate, and violent
+    85, 86	         Snow showers slight and heavy
+    95 *	         Thunderstorm: Slight or moderate
+    96, 99 *	     Thunderstorm with slight and heavy hail
+ */
