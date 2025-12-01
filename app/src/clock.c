9902e824fe50914100dc5f8e772bc156aeff13bd
kbph05 <horvatk@shaw.ca>
2025-12-01 14:54:24 -0800

    initialized all values in clock.c

--- app/src/clock.c
+++ app/src/clock.c
@@ -8,6 +8,11 @@ rtc_t* initClock() {
     rtc_t *clock = malloc(sizeof(rtc_t));
     clock->hour = 0;
     clock->min = 0; 
+    clock->month = 0;
+    clock->day = 0;
+    clock->dotw = 0;
+    clock->year = 0;
+    clock->isPm = 0;
     return clock;
 }
 
