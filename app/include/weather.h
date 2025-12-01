4417f6a6d7b3bb494745a816b7755a60a717dd20
kbph05 <horvatk@shaw.ca>
2025-11-30 16:33:20 -0800

    can get daily weather, still working on weather codes

--- app/include/weather.h
+++ app/include/weather.h
@@ -4,6 +4,25 @@
  * 
  */
 
+
+// Weather code table (according to the openmeteo website)
+/**
+ * Code	            Description
+    0	             Clear sky
+    1, 2, 3	         Mainly clear, partly cloudy, and overcast
+    45, 48	         Fog and depositing rime fog
+    51, 53, 55	     Drizzle: Light, moderate, and dense intensity
+    56, 57	         Freezing Drizzle: Light and dense intensity
+    61, 63, 65	     Rain: Slight, moderate and heavy intensity
+    66, 67	         Freezing Rain: Light and heavy intensity
+    71, 73, 75	     Snow fall: Slight, moderate, and heavy intensity
+    77	             Snow grains
+    80, 81, 82	     Rain showers: Slight, moderate, and violent
+    85, 86	         Snow showers slight and heavy
+    95 *	         Thunderstorm: Slight or moderate
+    96, 99 *	     Thunderstorm with slight and heavy hail
+ */
+
 #ifndef WEATHER_H
 #define WEATHER_H
 
@@ -17,8 +36,15 @@
  * @brief The weather data struct. Contains an array of doubles for the temperatures and the number of elements in the array
  * 
  */
+
 typedef struct {
-    double *temps;
+    double daily_temp;
+    double conditions;
+} Daily;
+
+typedef struct {
+    Daily *daily;
+    int daily_count;
     int count;
 } WeatherData;
 
